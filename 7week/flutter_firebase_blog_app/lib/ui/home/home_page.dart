e8a6319a95213b37ddccdfb1f0385c454315c52c
jangbogyeong <j_3827@naver.com>
2025-04-21 20:20:53 +0900

    8week 1day

--- 7week/flutter_firebase_blog_app/lib/ui/home/home_page.dart
+++ 7week/flutter_firebase_blog_app/lib/ui/home/home_page.dart
@@ -4,23 +4,86 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('BLOG')),
-      body: Column(
-        children: [
-          //
-          Text('최근글', style: TextStyle(fontWeight: FontWeight.bold)),
-          Expanded(
-            child: ListView(
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: 120,
-                  color: Colors.blue,
-                ),
-              ],
+      appBar: AppBar(title: Text('BLOG')), //appbar
+      backgroundColor: Colors.grey[200],
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //
+            Text('최근글', style: TextStyle(fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 120,
+                    child: Stack(
+                      children: [
+                        //
+                        Positioned(
+                          right: 0,
+                          width: 120,
+                          height: 120,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.network(
+                              'https://picsum.photos/200/300',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          margin: EdgeInsets.only(right: 100),
+                          padding: EdgeInsets.all(20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Today I Learned',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14,
+                                ),
+                              ),
+                              Spacer(),
+                              Text(
+                                'Flutter 그리드뷰를 배웠습니다.Flutter 그리드뷰를 배웠습니다.',
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14,
+                                ),
+                              ),
+                              SizedBox(height: 4),
+                              Text(
+                                '2025.04.21  20:15',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- 7week/flutter_riverpod_mvvm/android/app/src/main/kotlin/com/example/flutter_riverpod_mvvm/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_riverpod_mvvm
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
